447a71ba1a035d637b2efc4ff54a27a6dcc1ae63
skywolf46 <skywolf461@gmail.com>
2022-07-11 07:27:40 +0900

    fix: add plugin classloader to class registration logic

--- src/main/kotlin/skywolf46/extrautil/bukkit/ExtraUtilPlugin.kt
+++ src/main/kotlin/skywolf46/extrautil/bukkit/ExtraUtilPlugin.kt
@@ -1,10 +1,17 @@
 package skywolf46.extrautil.bukkit
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import skywolf46.extrautil.bukkit.annotations.AllowScanning
 import skywolf46.extrautility.core.ExtraUtilityCore
+import skywolf46.extrautility.core.util.ReflectionUtil
 
 class ExtraUtilPlugin : JavaPlugin() {
     override fun onEnable() {
+        for (x in Bukkit.getPluginManager().plugins) {
+            if (x.javaClass.getAnnotation(AllowScanning::class.java) != null)
+                ReflectionUtil.addClassLoader(x.javaClass.classLoader)
+        }
         ExtraUtilityCore.init()
     }
 }
---
+++ src/main/kotlin/skywolf46/extrautil/bukkit/annotations/AllowScanning.kt
@@ -0,0 +1,5 @@
+package skywolf46.extrautil.bukkit.annotations
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AllowScanning
